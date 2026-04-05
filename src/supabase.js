c88b4907d7ff9489aba23fed07ce906f7d1c93ce
repojun <tytal090@gmail.com>
2026-04-05 14:29:52 +0100

    troubleshooting vercel

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
+// grab env vars
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase Missing environment variables!");
+}
+
+// create client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
