07cb90aaa123145c8952421a0b47674b9159e1fa
repojun <tytal090@gmail.com>
2026-08-03 16:33:34 +0100

    adding voice-sync back (oops, sorry cxla lol)

--- src/assets/components/Content/Projects.js
+++ src/assets/components/Content/Projects.js
@@ -90,6 +90,26 @@ I spent countless hours adding various features, that have gone way above the or
     creator: true,
   },
   {
+    id: "voice-sync",
+    name: "VoiceSync",
+    description: `
+I always had issues with Discord VC bots. Most of them felt generic, locked simple features behind voting or payments, and never really solved the problems I actually dealt with. Trolls blasting loud sounds through their mic, people joining just to annoy others, or having no real control over who could come back into my voice channels always frustrated me.
+
+A lot of the features came from things I personally wished existed. Permanent autobans for trolls, temporary moderation so friends could help manage a VC, following people between channels instead of scrolling through hundreds of voice calls trying to find them, all of it was built from experiences I had myself.
+
+A huge inspiration for me was the old internet and classic PC games. I've always loved the Frutiger Aero aesthetic and games like Heavy Weapon Deluxe and Plants vs. Zombies. Everything felt colorful, smooth, expressive, and actually fun to use. Software back then had personality, and I wanted to bring some of that feeling into what I was making instead of creating another boring bot.
+
+More than anything, I just wanted voice channels to feel smoother, private and easier to control.
+
+`,
+    link: "https://modsync.app/",
+    image: "/projects/voicesync.webp",
+    author: "Cxla",
+    category: "Discord Bot",
+    date: "15/01/26",
+    creator: true,
+  },
+  {
     id: "perfect-hue",
     name: "Perfect Hue",
     description: `
